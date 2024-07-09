3e0b8ed04f06218317f6f9b2e97281d99b05668d
Akshat Singhal <77186043+AkshatSinghal1709@users.noreply.github.com>
2024-07-09 14:15:10 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,11 +1,16 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.sql import func
 from .database import Base
 
-class Configuration(Base):
-    __tablename__ = 'configurations'
+class SearchSystem(Base):
+    __tablename__ = 'searchv2'
 
     id = Column(Integer, primary_key=True, index=True)
-    country_code = Column(String, unique=True, index=True)
-    business_name = Column(String, nullable=False)
-    registration_number = Column(String, nullable=True)
-    additional_details = Column(String, nullable=True)
+    origin = Column(String, index=True, nullable=False)
+    destination = Column(String, nullable=False)
+    size = Column(String, nullable=False)
+    type = Column(String, nullable=False)
+    commodity = Column(String, nullable=False)
+    count = Column(Integer, nullable=False)
+    created_at = Column(DateTime, default=func.now(), nullable=False)
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now(), nullable=False)
