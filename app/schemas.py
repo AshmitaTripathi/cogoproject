14bcd75d04f4288dd58a8a0b64c4d3508dd66f91
Akshat Singhal <snglakshat@gmail.com>
2024-07-15 10:31:41 +0530

    Corrected the query params into a json dict , added total_count

--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel
 from datetime import datetime
+from typing import Optional , List
+from datetime import date
 
 
 class SearchV2Base(BaseModel):
@@ -9,6 +11,18 @@ class SearchV2Base(BaseModel):
     type : str
     commodity : str
     count : int
+
+
+class SearchQuery(BaseModel):
+    page: int=1
+    page_size: int=5
+    origin: Optional[str] = None
+    destination: Optional[str] = None
+    size: Optional[str] = None
+    type: Optional[str] = None
+    commodity: Optional[str] = None
+    sort_by: Optional[str] = "updated_at"
+    sort_type: Optional[str] = "desc"
 
 class SearchV2Create(SearchV2Base):
     pass
@@ -26,3 +40,11 @@ class SearchV2(SearchV2Base):
 
     class Config:
         orm_mode = True
+
+
+class PaginatedSearchResponse(BaseModel):
+    list: List[SearchV2]
+    page: int
+    total_count: int
+    page_size: int
+    total_pages: int
