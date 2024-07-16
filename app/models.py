8022145aad5f2a6e8c7080e3a47fa13c27897214
Akshat Singhal <snglakshat@gmail.com>
2024-07-16 12:47:29 +0530

    added complex models

--- app/models.py
+++ app/models.py
@@ -1,16 +1,74 @@
-from sqlalchemy import Column, Integer, String, DateTime
+# from sqlalchemy import Column, Integer, String, DateTime
+# from sqlalchemy.sql import func
+# from .database import Base
+
+# class SearchSystem(Base):
+#     __tablename__ = 'searchv2'
+
+#     id = Column(Integer, primary_key=True, index=True)
+#     origin = Column(String, index=True, nullable=False)
+#     destination = Column(String, nullable=False)
+#     size = Column(String, nullable=False)
+#     type = Column(String, nullable=False)
+#     commodity = Column(String, nullable=False)
+#     count = Column(Integer, nullable=True)
+#     created_at = Column(DateTime, default=func.now(), nullable=False)
+#     updated_at = Column(DateTime, default=func.now(), onupdate=func.now(), nullable=False)
+
+
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey , Enum
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from .database import Base
+import enum
 
-class SearchSystem(Base):
-    __tablename__ = 'searchv2'
+
+class ServiceTypeEnum(enum.Enum):
+    FCL = "FCL"
+    AIR = "AIR"
+
+class SearchSystemBase(Base):
+    __tablename__ = 'searchv22'
 
     id = Column(Integer, primary_key=True, index=True)
     origin = Column(String, index=True, nullable=False)
     destination = Column(String, nullable=False)
+    service_type = Column(Enum(ServiceTypeEnum), nullable=False)
+    created_at = Column(DateTime, default=func.now(), nullable=False)
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now(), nullable=False)
+
+    fcl = relationship("FCL", back_populates="search")
+    air = relationship("AIR", back_populates="search")
+
+   
+
+
+class FCL(Base):
+    __tablename__ = 'fcll'
+
+    id = Column(Integer, primary_key=True, index=True)
+    search_id = Column(Integer, ForeignKey('searchv22.id'))
     size = Column(String, nullable=False)
     type = Column(String, nullable=False)
     commodity = Column(String, nullable=False)
     count = Column(Integer, nullable=True)
-    created_at = Column(DateTime, default=func.now(), nullable=False)
-    updated_at = Column(DateTime, default=func.now(), onupdate=func.now(), nullable=False)
+
+    search = relationship("SearchSystemBase", back_populates="fcl")
+
+
+class AIR(Base):
+    __tablename__ = 'airr'
+
+    id = Column(Integer, primary_key=True, index=True)
+    search_id = Column(Integer, ForeignKey('searchv22.id'))
+    cargo_date = Column(DateTime, nullable=False)
+    commodity = Column(String, nullable=False)
+    sub_commodity = Column(String, nullable=False)
+    type = Column(String, nullable=False)
+    package_type = Column(String, nullable=False)
+    no_of_units = Column(Integer, nullable=False)
+    tot_vol = Column(Integer, nullable=False)
+    tot_weight = Column(Integer, nullable=False)
+    handling = Column(String, nullable=True)
+
+    search = relationship("SearchSystemBase", back_populates="air")
--- app/schemas.py
+++ app/schemas.py
@@ -1,49 +1,128 @@
+# from pydantic import BaseModel
+# from datetime import datetime
+# from typing import Optional , List
+# from datetime import date
+
+
+# class SearchV2Base(BaseModel):
+#     origin : str
+#     destination : str
+#     size : str
+#     type : str
+#     commodity : str
+#     count : int
+
+
+# class SearchQuery(BaseModel):
+#     page: int=1
+#     page_size: int=5
+#     origin: Optional[str] = None
+#     destination: Optional[str] = None
+#     size: Optional[str] = None
+#     type: Optional[str] = None
+#     commodity: Optional[str] = None
+#     sort_by: Optional[str] = "updated_at"
+#     sort_type: Optional[str] = "desc"
+
+# class SearchV2Create(SearchV2Base):
+#     pass
+
+# class SearchV2Update(SearchV2Base):
+#     id: int
+#     created_at: datetime
+#     updated_at: datetime
+#     pass
+
+# class SearchV2(SearchV2Base):
+#     id: int
+#     created_at: datetime
+#     updated_at: datetime
+
+#     class Config:
+#         orm_mode = True
+
+
+# class PaginatedSearchResponse(BaseModel):
+#     list: List[SearchV2]
+#     page: int
+#     total_count: int
+#     page_size: int
+#     total_pages: int
+
 from pydantic import BaseModel
 from datetime import datetime
-from typing import Optional , List
-from datetime import date
+from typing import Optional, List
+from enum import Enum
+
+class ServiceTypeEnum(str, Enum):
+    FCL = "FCL"
+    AIR = "AIR"
 
 
-class SearchV2Base(BaseModel):
-    origin : str
-    destination : str
-    size : str
-    type : str
-    commodity : str
-    count : int
-
-
-class SearchQuery(BaseModel):
-    page: int=1
-    page_size: int=5
-    origin: Optional[str] = None
-    destination: Optional[str] = None
-    size: Optional[str] = None
-    type: Optional[str] = None
-    commodity: Optional[str] = None
-    sort_by: Optional[str] = "updated_at"
-    sort_type: Optional[str] = "desc"
-
-class SearchV2Create(SearchV2Base):
-    pass
-
-class SearchV2Update(SearchV2Base):
-    id: int
-    created_at: datetime
-    updated_at: datetime
-    pass
-
-class SearchV2(SearchV2Base):
-    id: int
-    created_at: datetime
-    updated_at: datetime
+class SearchSystemBase(BaseModel):
+    origin: str
+    destination: str
+    service_type: ServiceTypeEnum
 
     class Config:
         orm_mode = True
 
+class FCLBase(BaseModel):
+    size: str
+    type: str
+    commodity: str
+    count: Optional[int] = None
+
+class FCLCreate(FCLBase):
+    search_id: int
+
+class FCL(FCLBase):
+    id: int
+   
+
+    class Config:
+        orm_mode = True
+
+class FCLResponse(FCL):
+    origin: str
+    destination: str
+
+    class Config:
+        orm_mode = True
+
+class AIRBase(BaseModel):
+    cargo_date: datetime
+    commodity: str
+    sub_commodity: str
+    type: str
+    package_type: str
+    no_of_units: int
+    tot_vol: int
+    tot_weight: int
+    handling: Optional[str] = None
+
+class AIRCreate(AIRBase):
+    search_id: int
+
+class AIR(AIRBase):
+    id: int 
+
+    class Config:
+        orm_mode = True
+
+class AIRResponse(AIR):
+    origin: str
+    destination: str
+
+    class Config:
+        orm_mode = True
+
+class SearchSystemWithDetails(SearchSystemBase):
+    fcl: Optional[List[FCL]] = []
+    air: Optional[List[AIR]] = []
 
 class PaginatedSearchResponse(BaseModel):
-    list: List[SearchV2]
+    list: List[SearchSystemWithDetails]
     page: int
     total_count: int
     page_size: int
